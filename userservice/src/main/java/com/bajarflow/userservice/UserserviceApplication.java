80afb19d7d276dce6fb53c0b6c17376edec23596
saifanwar1 <saifanwar2512001@gmail.com>
2025-08-14 21:01:03 +0530

    added one method

--- userservice/src/main/java/com/bajarflow/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/bajarflow/userservice/UserserviceApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserserviceApplication {
 
+	public void project(){
+
+		System.out.println("hello");
+	}
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
 	}
